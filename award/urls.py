2fbdb7a4fd48918885b183db06804d15603e1519
Kemunto Cindy <cindy.kemu@student.moringaschool.com>
2022-06-13 17:42:04 +0300

    Include a path for the profile

--- award/urls.py
+++ award/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import index, RegisterView
+from .views import index, RegisterView, profile
 
 urlpatterns = [
     path('', index, name='index'),
     path('register/', RegisterView.as_view(), name='register'),
+    path('profile/', profile, name='users-profile'),
 ]
