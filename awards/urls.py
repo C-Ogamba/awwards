e13ea5707474d761bba1388e11dd2a88f39201cc
Kemunto Cindy <cindy.kemu@student.moringaschool.com>
2022-06-13 04:37:56 +0300

    Dev:update our routes

--- awards/urls.py
+++ awards/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('award/', include('award.urls')),
+    path('', include('award.urls')),
     path('admin/', admin.site.urls),
 ]
