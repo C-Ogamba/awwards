bc89f7af6a07cdbc47be60ed8cd9d83dfa75ca12
Kemunto Cindy <cindy.kemu@student.moringaschool.com>
2022-06-13 08:45:26 +0300

    Fix: remove redundant code

--- award/forms.py
+++ award/forms.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 class RegisterForm(UserCreationForm):
-    first_name = forms.CharField(max_lenth=100, required=True, widget=forms.TextInput(attrs={'placeholder':'First Name', 'class':'form-control'}))
+    first_name = forms.CharField(max_length=100, required=True, widget=forms.TextInput(attrs={'placeholder':'First Name', 'class':'form-control'}))
     last_name = forms.CharField(max_length=100, required=True, widget=forms.TextInput(attrs={'placeholder': 'Last Name', 'class': 'form-control'}))
     username = forms.CharField(max_length=100, required=True, widget=forms.TextInput(attrs={'placeholder': 'Username', 'class': 'form-control'}))
     email = forms.EmailField(required=True, widget=forms.TextInput(attrs={'placeholder': 'Email', 'class': 'form-control'}))
