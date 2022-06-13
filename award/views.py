d1d7f13da5a592f3095d2d8a4b53ca28f05c7852
Kemunto Cindy <cindy.kemu@student.moringaschool.com>
2022-06-13 04:39:59 +0300

    Dev: make changesin the view file

--- award/views.py
+++ award/views.py
@@ -4,5 +4,5 @@ from django.shortcuts import render
 # Create your views here.
 
 
-def home(request):
-    return render(request, 'award/index.html')
+def index(request):
+    return render(request, 'users/index.html')
