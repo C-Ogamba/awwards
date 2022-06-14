34d80602c45352e835622cd94ab633153cc3b1c4
Kemunto Cindy <cindy.kemu@student.moringaschool.com>
2022-06-14 08:45:33 +0300

    Fix: add and change redundant code

--- award/views.py
+++ award/views.py
@@ -1,12 +1,15 @@
-from django.shortcuts import render, redirect 
+from django.shortcuts import render, get_object_or_404, redirect 
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.views import View
 
 from django.contrib.auth.views import LoginView
-from .forms import RegisterForm, LoginForm
-from django.urls import reverse_lazy
-from django.contrib.auth.views import PasswordResetView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.http import HttpResponseRedirect
+from award.models import Projects
+from .forms import RegisterForm, LoginForm, UpdateUserForm, UpdateProfileForm
+from django.urls import reverse_lazy,reverse
+from django.contrib.auth.views import PasswordResetView, PasswordChangeView
 from django.contrib.messages.views import SuccessMessageMixin
 
 # Create your views here.
@@ -72,4 +75,86 @@ class ResetPasswordView(SuccessMessageMixin, PasswordResetView):
 
 @login_required
 def profile(request):
-    return render(request, 'users/profile.html')
+    if request.method == 'POST':
+        # profile = UserProfile.objects.create(user=request.user)
+        user_form = UpdateUserForm(request.POST, instance=request.user)
+        profile_form = UpdateProfileForm(request.POST, request.FILES, instance=request.user.profile)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, 'Your profile is updated successfully')
+            return redirect(to='users-profile')
+    else:
+        user_form = UpdateUserForm(instance=request.user)
+        profile_form = UpdateProfileForm(instance=request.user.profile)
+
+    return render(request, 'users/profile.html', {'user_form': user_form, 'profile_form': profile_form})
+
+class ChangePasswordView(SuccessMessageMixin, PasswordChangeView):
+    template_name = 'users/change_password.html'
+    success_message = "Successfully Changed Your Password"
+    success_url = reverse_lazy('index')
+
+def Search(request):
+    if request.method == "POST":
+        searched = request.POST['searched']
+        projects = Projects.objects.filter(title__icontains=searched)
+        return render(request, 'search.html', {'searched':searched, 'posts': posts})
+
+    else:
+        return render(request, 'search.html', {})
+
+class HomeView(ListView):
+    model = Projects
+    # template_name = 'index.html'
+    # ordering = ['-post_date']
+    def get(self, request):
+        posts = Projects.objects.order_by('-post_date')[:20]
+        ctx = {
+            'object_list': posts
+        }
+        return render(request, 'index.html', ctx)
+
+def CategoryView(request, cats):
+    category_posts = Projects.objects.filter(category=cats)
+    return render(request, 'categories.html', {'cats':cats.title(),'category_posts':category_posts })
+
+def LikeView(request,pk):
+    post =  get_object_or_404(Projects, id=request.POST.get('post_id'))
+    liked = False
+    if post.likes.filter(id=request.user.id).exists():
+        post.likes.remove(request.user)
+        liked = False
+    else:
+        post.likes.add(request.user)
+        liked = True
+    
+    return HttpResponseRedirect(reverse('article-detail', args=[str(pk)]))
+
+
+
+class AddPostView(CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'add_post.html'
+    # fields = '__all__'
+    # fields = ('title', 'body')
+
+class AddCategoryView(CreateView):
+    model = Category
+    # form_class = PostForm
+    template_name = 'add_category.html'
+    fields = '__all__'
+    # fields = ('title', 'body')
+
+class UpdatePostView(UpdateView):
+    model = Post
+    form_class = EditForm
+    template_name = 'update_post.html'
+    # fields = ['title', 'title_tag', 'body']
+
+class DeletePostView(DeleteView):
+    model = Post
+    template_name = 'delete_post.html'
+    success_url = reverse_lazy('home')
