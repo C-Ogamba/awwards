5a870a3c72679fa18f82e2cf9d5c686fb9858d4f
Kemunto Cindy <cindy.kemu@student.moringaschool.com>
2022-06-15 21:48:54 +0300

    Dev: update the views file

--- award/views.py
+++ award/views.py
@@ -2,19 +2,47 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.views import View
-from django.views.generic import DetailView, CreateView, UpdateView, DeleteView
+from django.views.generic import ListView,DetailView, CreateView, UpdateView, DeleteView
 
 from django.contrib.auth.views import LoginView
-from .models import Post, Profile
-from .forms import PostForm, EditForm
-from .forms import RegisterForm, LoginForm, UpdateUserForm, UpdateProfileForm
-from django.urls import reverse_lazy,reverse
+from .models import Post, Profile, Rating
+from .forms import RegisterForm, LoginForm, UpdateProfileForm,PostForm, EditForm, RatingsForm
+from django.urls import reverse_lazy
 from django.contrib.auth.views import PasswordResetView, PasswordChangeView
 from django.contrib.messages.views import SuccessMessageMixin
+from django.http import  HttpResponseRedirect
+import random
 
 # Create your views here.
 def index(request):
-    return render(request, 'users/index.html')
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = request.user
+            post.save()
+    else:
+        form = PostForm()
+    try:
+        posts = Post.objects.all()
+        posts = posts[::-1]
+        a_post = random.randint(0,len(posts)-1)
+        random_post = posts[a_post]
+        print(random_post.photo)
+    except Post.DoesNotExist:
+        posts = None
+    return render(request, 'users/index.html', {'posts': posts, 'form': form, 'random_post': random_post})
+
+class HomeView(ListView):
+    model = Post
+    # template_name = 'index.html'
+    # ordering = ['-post_date']
+    def get(self, request):
+        posts = Post.objects.order_by('-post_date')[:20]
+        ctx = {
+            'object_list': posts
+        }
+        return render(request, 'users/index.html', ctx)
 
 def dispatch(self, request, *args, **kwargs):
         # will redirect to the home page if a user tries to access the register page while logged in
@@ -82,6 +110,7 @@ def profile(request):
 
     return render (request, 'users/profile.html', {"profile":profile, "current_user":current_user})
 
+@login_required
 def update_profile(request):
     current_user = request.user
     if request.method == 'POST':
@@ -90,28 +119,52 @@ def update_profile(request):
             profile = form.save(commit = False)
             profile.save()
 
-        return redirect("profile", id = current_user.id )   
+        return redirect("users-profile" )   
 
     else:
         form = UpdateProfileForm()  
 
     return render(request, 'users/update_profile.html', {"current_user":current_user , "form":form})
 
-    # if request.method == 'POST':
-    #     # profile = UserProfile.objects.create(user=request.user)
-    #     user_form = UpdateUserForm(request.POST, instance=request.user)
-    #     profile_form = UpdateProfileForm(request.POST, request.FILES, instance=request.Profile.user)
-
-    #     if user_form.is_valid() and profile_form.is_valid():
-    #         user_form.save()
-    #         profile_form.save()
-    #         messages.success(request, 'Your profile is updated successfully')
-    #         return redirect(to='users-profile')
-    # else:
-    #     user_form = UpdateUserForm(instance=request.user)
-    #     profile_form = UpdateProfileForm(instance=request.Profile.user)
-
-    # return render(request, 'users/profile.html', {'user_form': user_form, 'profile_form': profile_form})
+@login_required
+def project(request, post):
+    post = Post.objects.get(title=post)
+    ratings = Rating.objects.filter(user=request.user, post=post).first()
+    rating_status = None
+    if ratings is None:
+        rating_status = False
+    else:
+        rating_status = True
+    if request.method == 'POST':
+        form = RatingsForm(request.POST)
+        if form.is_valid():
+            rate = form.save(commit=False)
+            rate.user = request.user
+            rate.post = post
+            rate.save()
+            post_ratings = Rating.objects.filter(post=post)
+            design_ratings = [d.design for d in post_ratings]
+            design_average = sum(design_ratings) / len(design_ratings)
+            usability_ratings = [us.usability for us in post_ratings]
+            usability_average = sum(usability_ratings) / len(usability_ratings)
+            content_ratings = [content.content for content in post_ratings]
+            content_average = sum(content_ratings) / len(content_ratings)
+            score = (design_average + usability_average + content_average) / 3
+            print(score)
+            rate.design_average = round(design_average, 2)
+            rate.usability_average = round(usability_average, 2)
+            rate.content_average = round(content_average, 2)
+            rate.score = round(score, 2)
+            rate.save()
+            return HttpResponseRedirect(request.path_info)
+    else:
+        form = RatingsForm()
+    params = {
+        'post': post,
+        'rating_form': form,
+        'rating_status': rating_status
+    }
+    return render(request, 'users/article_detail.html', params)
 
 class ChangePasswordView(SuccessMessageMixin, PasswordChangeView):
     template_name = 'users/change_password.html'
@@ -121,19 +174,10 @@ class ChangePasswordView(SuccessMessageMixin, PasswordChangeView):
 
 class ArticleDetailView(DetailView):
     model = Post
-    template_name = 'article_details.html'
+    template_name = 'users/article_details.html'
 
     def get_context_data(self, *args, **kwargs):
-        context = super(ArticleDetailView, self).get_context_data(*args, **kwargs)
-        stuff = get_object_or_404(Post, id=self.kwargs['pk'])
-        total_likes = stuff.total_likes()
-
-        liked = False
-        if stuff.likes.filter(id=self.request.user.id).exists():
-            liked = True
-
-        context["total_likes"] = total_likes
-        context["liked"] = liked
+        context = super(ArticleDetailView, self).get_context_data(*args, **kwargs)       
         return context
 
 
@@ -144,23 +188,17 @@ class AddPostView(CreateView):
     # fields = '__all__'
     # fields = ('title', 'body')
 
-# class AddCategoryView(CreateView):
-#     model = Category
-#     # form_class = PostForm
-#     template_name = 'add_category.html'
-#     fields = '__all__'
-#     # fields = ('title', 'body')
 
 class UpdatePostView(UpdateView):
     model = Post
     form_class = EditForm
-    template_name = 'update_post.html'
+    template_name = 'users/update_post.html'
     # fields = ['title', 'title_tag', 'body']
 
 class DeletePostView(DeleteView):
     model = Post
-    template_name = 'delete_post.html'
-    success_url = reverse_lazy('home')
+    template_name = 'users/delete_post.html'
+    success_url = reverse_lazy('index')
 
 def Search(request):
     if request.method == "POST":
