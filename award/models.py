8c6a084a454a252466facf92fd78346f7124608a
Kemunto Cindy <cindy.kemu@student.moringaschool.com>
2022-06-15 21:53:17 +0300

    Feat: update the models file

--- award/models.py
+++ award/models.py
@@ -3,6 +3,8 @@ from django.contrib.auth.models import User
 from PIL import Image
 from django.urls import reverse
 from django.db.models.signals import post_save
+from django.dispatch import receiver
+import datetime as dt
 
 # Create your models here.
 class Profile(models.Model):
@@ -24,16 +26,7 @@ class Profile(models.Model):
             img.thumbnail(new_img)
             img.save(self.avatar.path)
 
-# class Category(models.Model):
-#     name =models.CharField(max_length=255)
 
-
-#     def __str__(self):
-#         return self.name 
-
-#     def get_absolute_url(self):
-#         return reverse('home')
-        # return reverse('article-detail', args=(str(self.id)))
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
@@ -43,17 +36,14 @@ class Post(models.Model):
     post_date = models.DateField(auto_now_add=True)
     category = models.CharField(max_length=255, default='coding')
     image = models.ImageField(default='default.jpg', upload_to='images')
-    likes = models.ManyToManyField(User, related_name='blog_posts')
-    
-    def total_likes(self):
-        return self.likes.count()
+    link = models.URLField(max_length=200, null=True)
 
 
     def __str__(self):
         return self.title + '|' + str(self.author)
 
     def get_absolute_url(self):
-        return reverse('home')
+        return reverse('index')
         # return reverse('article-detail', args=(str(self.id)))
 
     def save_post(self):
@@ -61,7 +51,41 @@ class Post(models.Model):
 
     def update_caption(self, body):
         return self.body.update()
-        
+
 
     def delete_post(self):
         return self.delete()
+
+class Rating(models.Model):
+    rating = (
+        (1, '1'),
+        (2, '2'),
+        (3, '3'),
+        (4, '4'),
+        (5, '5'),
+        (6, '6'),
+        (7, '7'),
+        (8, '8'),
+        (9, '9'),
+        (10, '10'),
+    )
+    design = models.IntegerField(choices=rating, default=0, blank=True)
+    usability = models.IntegerField(choices=rating, blank=True)
+    content = models.IntegerField(choices=rating, blank=True)
+    score = models.FloatField(default=0, blank=True)
+    design_average = models.FloatField(default=0, blank=True)
+    usability_average = models.FloatField(default=0, blank=True)
+    content_average = models.FloatField(default=0, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, related_name='rater')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='ratings', null=True)
+    
+    def save_rating(self):
+        self.save()
+
+    @classmethod
+    def get_ratings(cls, id):
+        ratings = Rating.objects.filter(post_id=id).all()
+        return ratings
+
+    def __str__(self):
+        return f'{self.post} Rating'
